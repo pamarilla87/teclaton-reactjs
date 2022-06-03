d44c2f37a1cc3cc4accf456aa113077bd09261c8
pamarilla <dl.neurus@gmail.com>
2022-06-03 17:49:35 -0300

    Add searchbar to NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 //import logo from './logo.svg';
-import {BarraDeBusqueda} from './components/SearchBar/SearchBar'
+
 import { BarraDeNavegacion } from './components/Navbar/Navbar'
 import './App.css';
 
 function App() {
-  return (
-  <>
-  <BarraDeBusqueda />
-  <BarraDeNavegacion/>
-  </>
-)}
+  return <BarraDeNavegacion />
+}
 
 export default App;
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,32 +1,36 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './navbar.css'
-import { Navbar, Container, Nav, NavDropdown  } from 'react-bootstrap'
+import { BarraDeBusqueda } from '../SearchBar/SearchBar'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 
 
 export const BarraDeNavegacion = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" variant="light" bg="primary">
-            <Container>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg--primary"/>
-                <Navbar.Collapse id="basic-navbar-nav" className="navBarText navBarPosition">
-                    <Nav>
-                        <Nav.Link href="#home" className="ps-3 pe-5 nav-links">Inicio</Nav.Link>
-                        <Nav.Link href="#build" className="ps-3 pe-5 nav-links">Arma tu PC</Nav.Link>
-                        <NavDropdown title="Productos" id="basic-nav-dropdown"  className="ps-3 pe-5">
-                            <NavDropdown.Item href="#action/3.1" className="nav-links ">Procesadores</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2" className="nav-links">Motherboards</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3" className="nav-links">Placas de video</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.4" className="nav-links">Memorias</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.5" className="nav-links">Almacenamiento</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.6" className="nav-links">Fuentes</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.7" className="nav-links">Gabinetes</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#help" className="ps-3 pe-5 nav-links">Ayuda</Nav.Link>
-                        <Nav.Link href="#sponsors" className="ps-3 pe-5 nav-links">Marcas Sponsor</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <>
+            <BarraDeBusqueda />
+            <Navbar collapseOnSelect expand="lg" variant="light" bg="primary">
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg--primary" />
+                    <Navbar.Collapse id="basic-navbar-nav" className="navBarText navBarPosition">
+                        <Nav>
+                            <Nav.Link href="#home" className="ps-3 pe-5 nav-links">Inicio</Nav.Link>
+                            <Nav.Link href="#build" className="ps-3 pe-5 nav-links">Arma tu PC</Nav.Link>
+                            <NavDropdown title="Productos" id="basic-nav-dropdown" className="ps-3 pe-5">
+                                <NavDropdown.Item href="#action/3.1" className="nav-links ">Procesadores</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2" className="nav-links">Motherboards</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3" className="nav-links">Placas de video</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.4" className="nav-links">Memorias</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.5" className="nav-links">Almacenamiento</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.6" className="nav-links">Fuentes</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.7" className="nav-links">Gabinetes</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#help" className="ps-3 pe-5 nav-links">Ayuda</Nav.Link>
+                            <Nav.Link href="#sponsors" className="ps-3 pe-5 nav-links">Marcas Sponsor</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     )
 }
