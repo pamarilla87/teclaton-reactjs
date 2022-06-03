6b260b3e48d192df0f63f9a5aea88c3cdd0689d5
pamarilla <dl.neurus@gmail.com>
2022-06-03 20:45:07 -0300

    fix indentation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-//import logo from './logo.svg';
-
 import { BarraDeNavegacion } from './components/Navbar/Navbar'
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import './App.css';
 
 function App() {
-  return <BarraDeNavegacion />
+  return (
+    <>
+      <BarraDeNavegacion />
+      <ItemListContainer />
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export const ItemListContainer = () => {
+    return <h1>Estamos actualizando la lista de productos. Intente nuevamente mas tarde.</h1>
+}
